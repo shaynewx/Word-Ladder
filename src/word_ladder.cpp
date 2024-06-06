a575fc113d7818f7dedfef15ff99bc7bee3c5b1a
Xuan Wang <z5442066@ad.unsw.edu.au>
2024-06-06 13:12:04 +1000

    Finish read_lexicon

--- src/word_ladder.cpp
+++ src/word_ladder.cpp
@@ -1,10 +1,24 @@
 #include "word_ladder.h"
+#include <fstream> // for std::ifstream
+#include <stdexcept> // for std::runtime_error
+#include <queue> // for std::queue
 
 auto word_ladder::read_lexicon(const std::string& path) -> std::unordered_set<std::string> {
-	(void)path;
-	// remove this line once implemented
+	std::unordered_set<std::string> lexicon;
+	std::ifstream file(path); // open the file
+	if (!file) {
+		throw std::runtime_error("Unable to open file: " + path); // throw an error if the file cannot open
+	}
 
-	return {};
+	// insert each word of the line to the lexicon (skip every empty line)
+	std::string word;
+	while (std::getline(file, word)) {
+		if (!word.empty()) {
+			lexicon.emplace(std::move(word));
+		}
+	}
+
+	return lexicon;
 }
 
 auto word_ladder::generate(const std::string& from,
